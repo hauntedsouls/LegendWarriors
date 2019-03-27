c712a78628d47707f4f8aa8f00bd140753a827d4
gabespound <gabespound@gmail.com>
2019-03-26 17:03:36 -0700

    created UE Project in 4.22 preview 7

---
+++ Source/LegendWarriors/LegendWarriorsGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LegendWarriorsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEGENDWARRIORS_API ALegendWarriorsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
